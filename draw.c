49e938f47277f7217f62ddd8159f2f0d6df1f0ba
gummybuns <gummybuns@protonmail.com>
2025-08-07 23:06:28 +0000

    wip for adding the frequency domain to the project
    right now its using kiss_fft prolly wrong but i found
    a link to a homemade fft that removes the dependency
    so the goal is to leverage that. and for now it is
    only gonna be linear_se ill have to worry about
    transcoding later

--- draw.c
+++ draw.c
@@ -1,5 +1,8 @@
 #include <curses.h>
 #include <stdlib.h>
+#include <math.h>
+
+#include "kiss_fft.h"
 
 #include "audio_ctrl.h"
 #include "audio_rms.h"
@@ -73,6 +76,121 @@ draw_info(audio_ctrl_t ctrl, audio_stream_t audio_stream)
 		}
 	}
 }
+
+inline int
+*to_normalized_pcm(void *full_sample, float *pcm, audio_stream_t *audio_stream)
+{
+	u_int encoding, i, precision, total_samples;
+	char *cdata;
+	short *sdata;
+	float *fdata;
+
+	precision = audio_stream->precision;
+	total_samples = audio_stream->total_samples;
+	encoding = audio_stream->encoding;
+
+	switch (encoding) {
+	/* TODO assuming that the endian-ness matches the cpu */
+	case CTRL_SLINEAR_LE:
+	case CTRL_SLINEAR_BE:
+		if (precision == 8) {
+			cdata = (char *)full_sample;
+			for (i = 0; i < total_samples; i++) {
+				pcm[i] = (float)full_sample[i] / 128.0f;
+			}
+		} else if (precision == 16) {
+			sdata = (short *)full_sample;
+			for (i = 0; i < total_samples; i++) {
+				pcm[i] = (float)full_sample[i] / 32768.0f;
+			}
+		} else if (precision == 32) {
+			for (i = 0; i < total_samples; i++) {
+				pcm[i] = (float)full_sample[i] / 2147483647.0f;
+			}
+		} else {
+			return E_FREQ_UNKNOWN_PRECISION;
+		}
+	default:
+		return E_FREQ_UNSUPPORTED_ENCODING
+	return 0;
+}
+
+
+int
+draw_frequency(audio_ctrl_t ctrl, audio_stream_t *audio_stream)
+{
+	char keypress;
+	int res;
+	void *full_sample;
+	float *pcm;
+	/*
+	 * We have the total_samples (audio_stream.total_samples)
+	 * we have the size of the fft (1024 hard coded)
+	 * therefore there are total_samples / 1024 fft_frames we are going to compute
+	 * every frame has a start_idx / end_idx. that tells us where to look in the buffer
+	 */
+	u_int bars = 32;
+	u_int fft_size = 1024;
+	u_int bins = fft_size / 2;
+	u_int frames = audio_stream->total_samples / fft_size;
+	float magnitudes[frames][bins];
+	float avg[bins];
+	kiss_fft_cpx in[fft_size], out[fft_size];
+
+	for (;;) {
+		// todo - i need to zero out the avg after each loop
+		res = stream(ctrl, audio_stream);
+		if (res != 0) {
+			return res;
+		}
+
+		full_samples = flatten_stream(audio_stream);
+		pcm = malloc(sizeof(float) * audio_stream->total_samples);
+		if (to_normalized_pcm(full_samples, pcm, audio_stream) > 0) {
+			free(full_samples);
+			free(pcm);
+			return res;
+		}
+
+		for (int i = 0; i < frames; i++) {
+			int start = i * fft_size;
+
+			/* initialize the fft in data */
+			for (int j = 0; j < fft_size; j++) {
+				in[j].r = pcm[start+j];
+				in[j].i = 0.0f;
+			}
+
+			/* perform the fft */
+			kiss_fft_cfg cfg = kiss_fft_alloc(fft_size, 0, NULL, NULL);
+			kiss_fft(cfg, in, out);
+
+			/* get the magitudes for each bin in the current frame */
+			/* calculate the running average */
+			for (int j = 0; j < bins; j++) {
+				magnitudes[i][j] = sqrtf(out[j].r * out[j].r + out[j].i * out[j].i);
+			}
+		}
+
+		/* calculate the average across all frames */
+		for (int i = 0; i < frames; i ++) {
+			for (int j = 0; j < bins; j++) {
+				avg[j] += magnitudes[i][j];
+			}
+			// todo - use the rms instead of just the mean
+			avg[j] = avg[j] / frames;
+		}
+
+		/* calculate the average for each bar */
+
+		/* calculate the height based of each bar */
+
+		/* draw each bar */
+		free(pcm);
+		free(full_samples);
+	}
+}
+
 
 /*
  * Displays a screen to record audio and show the intensity based on the
